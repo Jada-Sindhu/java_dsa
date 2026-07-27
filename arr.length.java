1394c10a76985b9330994e37714f74e0ab640508
Jada-Sindhu <sindhuns02@gmail.com>
2026-07-27 22:04:31 +0530

    Add files via upload

---
+++ arr.length.java
@@ -0,0 +1,12 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int arr[][]={{2,5,4,5,9},{4,9,8,0,7}};
+		for(int i=0;i<=arr.length;i++){
+		    for(int j=0;j<=arr.length;j++){
+		        System.out.println(arr[i][j]);
+		    }
+		}
+		System.out.println();
+	}
+}
