41c19d247a168529a4ea33eb04be18ca8756eb83
Jada-Sindhu <sindhuns02@gmail.com>
2026-07-27 15:28:58 +0530

    Add files via upload

---
+++ map.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+  
+  Map<Integer,Integer>m=new HashMap<>();
+  m.put(1,20);
+  m.put(2,90);
+  System.out.println(m);
+ 
+  System.out.print(m.containsKey(1));
+	}
+}
